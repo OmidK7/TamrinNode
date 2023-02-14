250b3b8f41290bb895a5d5af8df9dc5aa994308e
OmidK <omidk7914.ok@gmail.com>
2023-02-14 21:15:58 +0330

    tamrin03

--- tamrin03/Data.js
+++ tamrin03/Data.js
@@ -1,32 +1,27 @@
-const fs = require('fs')
+const fs = require("fs");
 
-let getData = ()=>{
-    return new Promise((resolve,reject)=>{
-        fs.readFile('./books.json',(err,data)=>{
-            
-            if(err){
-                reject(err)
-            }
+let getData = () => {
+  return new Promise((resolve, reject) => {
+    fs.readFile("./books.json", (err, data) => {
+      if (err) {
+        reject(err);
+      }
 
-            let stringData = Buffer.from(data).toString()
-            
-            resolve(JSON.parse(stringData))
+      let stringData = Buffer.from(data).toString();
+      resolve(JSON.parse(stringData));
+    });
+  });
+};
+let writeData = (dataIndx) => {
+  return new Promise((resolve, reject) => {
+    const dataWrite = JSON.stringify(dataIndx);
+    fs.writeFile("./books.json", dataWrite, (err, data) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(data);
+    });
+  });
+};
 
-        })
-    }) 
-
-}
-let writeData = (dataIndx)=>{
-    return new Promise((resolve,reject)=>{
-        const dataWrite = JSON.stringify(dataIndx)    
-        fs.writeFile('./books.json',dataWrite,(err , data)=>{
-            if(err){
-                reject(err)
-            }
-            resolve((data))
-        })
-    })
-
-}
-
-module.exports = {getData,writeData}
+module.exports = { getData, writeData };
